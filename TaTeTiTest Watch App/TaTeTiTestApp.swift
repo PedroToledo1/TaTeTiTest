dcc3c1e937a509c93b5270aa2d24682f07278cab
PedroToledo1 <59672144+PedroToledo1@users.noreply.github.com>
2023-08-04 18:29:52 -0300

    Initial Commit

---
+++ TaTeTiTest Watch App/TaTeTiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaTeTiTestApp.swift
+//  TaTeTiTest Watch App
+//
+//  Created by Pedro Toledo on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TaTeTiTest_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaTeTiTest/TaTeTiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaTeTiTestApp.swift
+//  TaTeTiTest
+//
+//  Created by Pedro Toledo on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TaTeTiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
