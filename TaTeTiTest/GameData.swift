108d50ba63ec28fbc3d4a6061e6a894f7a953f40
PedroToledo1 <59672144+PedroToledo1@users.noreply.github.com>
2023-08-05 20:01:13 -0300

    Update GameData.swift

--- TaTeTiTest/GameData.swift
+++ TaTeTiTest/GameData.swift
@@ -34,13 +34,61 @@ class Juego: ObservableObject{
                 puntosZeros += 1
             }
             let winner = turnos == Tile.equis ? "Cruz" : "Zeros"
-            mensajeFinal = winner + "Gano!"
+            mensajeFinal = winner + " Gano!"
             PartidaFinalizada = true
         }else{
             turnos = turnos == Tile.equis ? Tile.zero : Tile.equis
         }
+        /*if(empate()){
+            mensajeFinal = "Empate"
+            PartidaFinalizada = true
+        }
+         */
     }
-    func partidaTerminada()->Bool{return false}
+    func empate(){
+       /* partidaTerminada()
+        if partidaTerminada() == false && (puntosEquis + puntosZeros == 9){
+            return true
+        }else {
+            return false
+        }
+        */
+        mensajeFinal = "Empate"
+        PartidaFinalizada = true
+    }
+    func partidaTerminada()->Bool{
+        if turnoEquis(0, 0) && turnoEquis(0,1) && turnoEquis(0,2){
+            return true
+        }
+        if turnoEquis(1, 0) && turnoEquis(1,1) && turnoEquis(1,2){
+            return true
+        }
+        if turnoEquis(2, 0) && turnoEquis(2,1) && turnoEquis(2,2){
+            return true
+        }
+        if turnoEquis(0, 0) && turnoEquis(1,0) && turnoEquis(2,0){
+            return true
+        }
+        if turnoEquis(0, 1) && turnoEquis(1,1) && turnoEquis(2,1){
+            return true
+        }
+        if turnoEquis(0, 2) && turnoEquis(1,2) && turnoEquis(2,2){
+            return true
+        }
+        if turnoEquis(0, 0) && turnoEquis(1,1) && turnoEquis(2,2){
+            return true
+        }
+        if turnoEquis(2, 0) && turnoEquis(1,1) && turnoEquis(0,2){
+            return true
+        }
+        if (puntosEquis + puntosZeros == 9){
+            empate()
+        }
+        return false
+    }
+    func turnoEquis(_ row: Int, _ column: Int)-> Bool{
+        return board[row][column].tile == turnos
+    }
     func resetBoard(){
         var tableroNuevo =  [[celda]]()
         
